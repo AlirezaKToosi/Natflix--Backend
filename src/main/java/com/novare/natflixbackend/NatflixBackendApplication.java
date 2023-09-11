74474dd641bbe249f8e73d6b988ecf5e909b11ff
Alireza Kafshdartoosi <71192730+AlirezaKToosi@users.noreply.github.com>
2023-09-11 12:26:11 +0200

    Initial Commit

---
+++ src/main/java/com/novare/natflixbackend/NatflixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.novare.natflixbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatflixBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatflixBackendApplication.class, args);
+	}
+
+}
